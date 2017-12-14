58e1b7503dff15742766ad3247e5c49f6121ae5a
Stuart Alldritt <s.k.alldritt@gmail.com>
2017-12-13 23:16:28 -0800

    Auto-detect J-Link (if installed)

--- env.ps1
+++ env.ps1
@@ -18,8 +18,72 @@ nuget.exe install "$WorkspaceRoot\nuget\packages.config" -ConfigFile "$Workspace
 Add-ToPath -path "$PSScriptRoot\nuget\packages\autom8ed.com.tools.cmake\bin"
 Add-ToPath -path "$PSScriptRoot\nuget\packages\autom8ed.com.tools.ninja"
 
-# TODO: Find a way to get this utility into a nuget package
-Add-ToPath -path "C:\Program Files (x86)\SEGGER\JLink_V620b"
+# Try to detect a J-Link installation
+# Check Program Files (x86) first
+$seggerPath = $null
+if (Test-Path -Path "C:\Program Files (x86)" -PathType Container)
+{
+	$seggerPath = ((Get-ChildItem -Path "C:\Program Files (x86)") | Where-Object {$_.Name -eq "SEGGER"})
+}
+
+if ($seggerPath -eq $null)
+{
+	if (Test-Path -Path "C:\Program Files" -PathType Container)
+	{
+		$seggerPath = ((Get-ChildItem -Path "C:\Program Files") | Where-Object {$_.Name -eq "SEGGER"})
+	}
+}
+
+if ($seggerPath -ne $null)
+{
+	$jlinkPath = $null
+
+	# found a SEGGER install folder. See if there are any J-Link installs there
+	if (($seggerPath -ne $null) -and ($seggerPath.Count -eq 1))
+	{
+		$jlinkVersions = ((Get-ChildItem -Path $seggerPath.FullName) | Where-Object {$_.Name.StartsWith("JLink_") })
+
+		$recentVersion = $null
+		foreach($version in $jlinkVersions)
+		{
+			if ($recentVersion -eq $null)
+			{
+				$recentVersion = $version
+				continue
+			}
+
+			if ($recentVersion -lt $version)
+			{
+				$recentVersion = $version
+			}
+		}
+		
+		if ($recentVersion -ne $null)
+		{
+			$jlinkPath = $recentVersion.FullName
+		}
+	}
+	elseif ($seggerPath.Count -gt 1)
+	{
+		Write-Error "Multiple SEGGER folders found in C:\Program Files (x86)"
+	}
+
+	if ($jlinkPath -ne $null)
+	{
+		Write-Host "Using SEGGER install from $jlinkPath"
+		Add-ToPath -path $jlinkPath
+	}
+	else 
+	{
+		Write-Warning "Could not find JLink install under $seggerPath. Live debugging and MCU Flashing commands will be unavailable."
+		Write-Warning "Install J-Link from SEGGER Website"
+	}
+}
+else
+{
+	Write-Warning "Could not find SEGGER folder. Live debugging and MCU Flashing commands will be unavailable."
+	Write-Warning "Install J-Link from SEGGER Website"
+}
 
 # Add a new environment variable, GCC_ROOT, which points to the root of the GCC pulled down by NuGet
 # This is needed because Cmake toolchain files cannot import variables from other CMakeLists files, 
